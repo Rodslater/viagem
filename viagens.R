d8c2918c507177ffd564a2e07ba3319c47c1746c
Rodslater <91638825+Rodslater@users.noreply.github.com>
2023-03-23 18:03:10 -0300

    Create viagens.R

---
+++ viagens.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(downloader)
+library(lubridate)
+
+
+url <- "https://portaldatransparencia.gov.br/download-de-dados/viagens/2023"
+download(url, dest="dataset.zip", mode="wb") 
+unzip ("dataset.zip")
+
+viagens <- read.csv2("2023_Viagem.csv", dec =",", fileEncoding='latin1')
+file.remove(c('dataset.zip', '2023_Pagamento.csv', '2023_Passagem.csv', '2023_Trecho.csv', '2023_Viagem.csv'))
+
+IFS <- viagens %>%
+  filter(Código.órgão.solicitante == "26423",
+         Situação == 'Realizada') %>% 
+  select(nome=Nome, cargo=Cargo,inicio=Período...Data.de.início, fim=Período...Data.de.fim, destino=Destinos, valor_diaria=Valor.diárias, valor_passagem=Valor.passagens, urgencia=Viagem.Urgente) %>% 
+  mutate(inicio= dmy(inicio), fim= dmy(fim))
+
+
+saveRDS(IFS,'IFS.rds')
